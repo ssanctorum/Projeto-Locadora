45fe5a3344f7ab5461c5f6d65b7f71a5bbd17921
warlley <euwarlley@dominiolechuga.com>
2026-04-26 01:17:29 -0300

    separei os layouts do JOptionPane numa classe especifica pq assim fica mais facil, alem de usar static e final pra nao ter que instanciar toda vez

---
+++ src/Layouts_JOptionPane.java
@@ -0,0 +1,30 @@
+public class Layouts_JOptionPane {
+
+    public static final String menuPrincipal = "————— LOCADORA DE VEÍCULOS —————\n" +
+            "Escolha qual painel deseja utilizar:\n" +
+            "[1] - Cliente\n" +
+            "[2] - Funcionário\n" +
+            "[3] - Veículos\n" +
+            "[0] - Sair\n";
+
+    public static final String menuCliente = "————— LOCADORA DE VEÍCULOS —————\n"+
+            "— Painel do Cliente —\n" +
+            "[1] - Cadastrar-se\n" +
+            "[2] - Listar clientes\n" +
+            "[3] - Verificar cliente\n" +
+            "[0] - Voltar ao menu\n";
+
+    public static final String menuFuncionario = "————— LOCADORA DE VEÍCULOS —————\n"+
+            "— Painel do Funcionário —\n" +
+            "[1] - Admissão de funcionário\n" +
+            "[2] - Listar funcionários\n" +
+            "[3] - Remover funcionário\n" +
+            "[0] - Voltar\n";
+
+    public static final String menuVeiculo = "————— LOCADORA DE VEÍCULOS —————\n"+
+            "— Painel de Veículos —\n" +
+            "[1] - Cadastrar veículo\n" +
+            "[2] - Listar frota\n" +
+            "[3] - Pesquisar por Placa\n" +
+            "[0] - Voltar\n";
+}
