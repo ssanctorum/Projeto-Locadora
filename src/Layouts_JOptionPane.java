89ba4dbe66621d618def90a56c97c6c42f8a075e
warlley <euwarlley@dominiolechuga.com>
2026-04-26 03:18:40 -0300

    tentei centralizar os titulos

--- src/Layouts_JOptionPane.java
+++ src/Layouts_JOptionPane.java
@@ -8,21 +8,21 @@ public class Layouts_JOptionPane {
             "[0] - Sair\n\n";
 
     public static final String menuCliente = "————— LOCADORA DE VEÍCULOS —————\n"+
-            "\n— Painel do Cliente —\n" +
+            "\n                     —— Painel do Cliente ——\n" +
             "\n[1] - Cadastrar-se\n" +
             "[2] - Listar clientes\n" +
             "[3] - Verificar cliente\n" +
             "[0] - Voltar ao menu\n\n";
 
     public static final String menuFuncionario = "————— LOCADORA DE VEÍCULOS —————\n"+
-            "\n— Painel do Funcionário —\n" +
+            "\n                —— Painel do Funcionário ——\n" +
             "\n[1] - Admissão de funcionário\n" +
             "[2] - Listar funcionários\n" +
             "[3] - Remover funcionário\n" +
             "[0] - Voltar\n\n";
 
     public static final String menuVeiculo = "————— LOCADORA DE VEÍCULOS —————\n"+
-            "\n— Painel de Veículos —\n" +
+            "\n                   —— Painel de Veículos ——\n" +
             "\n[1] - Cadastrar veículo\n" +
             "[2] - Listar frota\n" +
             "[3] - Pesquisar por Placa\n" +
