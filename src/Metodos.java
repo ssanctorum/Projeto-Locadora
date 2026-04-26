771f8a9efb207bb67ee4507c98406809b099cceb
warlley <euwarlley@dominiolechuga.com>
2026-04-26 01:19:18 -0300

    comecei a trabalhar nos metodos de layout que vao chamar os outros metodos especificos de cada painel (cliente, funcionario e veiculo), alem disso deixei eles padrão (iguais) já que variaveis sao locais de cada metodo (mas ainda falta ajeitar umas coisinhas

--- src/Metodos.java
+++ src/Metodos.java
@@ -4,21 +4,144 @@ import java.util.ArrayList;
 public class Metodos {
 
 //essas são as tres listas principais
-    ArrayList<Cliente> clientes = new ArrayList<>();
-    ArrayList<Funcionario> funcionarios = new ArrayList<>();
-    ArrayList<Veiculo> veiculos = new ArrayList<>();
+    private ArrayList<Cliente> clientes = new ArrayList<>();
+    private ArrayList<Funcionario> funcionarios = new ArrayList<>();
+    private ArrayList<Veiculo> veiculos = new ArrayList<>();
 
-//aqui começam os metodos
-    public void menuCliente(JOptionPane jOptionPane){
-        //aqui vai ter opcao 1 - cadastro, opcao 2 - listar, opcao 3 - verificar
+    //aqui começam os metodos
+    //como os metodos possuem somente variaveis locais repetirei tudo
+
+    public void menuCliente(){
+
+        int loop = -1;
+
+        do {
+            String menuResposta = JOptionPane.showInputDialog(null, Layouts_JOptionPane.menuCliente, "Menu Principal", JOptionPane.QUESTION_MESSAGE);
+
+            //tratamentos que vão ser padrões abaixo
+            if (menuResposta == null){
+                return;
+            }
+
+            menuResposta = menuResposta.trim();
+            if (menuResposta.isEmpty()){
+                JOptionPane.showMessageDialog(null,"Você não digitou nada!");
+                continue;
+            }
+
+            int menuRespostaInt = Integer.parseInt(menuResposta);
+
+            switch (menuRespostaInt){
+
+                case 1:
+                    cadastroCliente();
+                    break;
+
+                case 2:
+                    listarCliente();
+                    break;
+
+                case 3:
+                    verificarCliente();
+                    break;
+
+                case 0:
+                    loop = 0;
+                    break;
+
+                default:
+                    JOptionPane.showMessageDialog(null, "Opção inválida! Tente novamente.");
+                    break;
+            }
+        } while (loop != 0);
     }
 
-    public void menuFuncionario(JOptionPane jOptionPane){
-        //aqui vai ter opcao 1 - cadastro, opcao 2 - listar
+    public void menuFuncionario(){
+        int loop = -1;
+
+        do {
+            String menuResposta = JOptionPane.showInputDialog(null, Layouts_JOptionPane.menuFuncionario, "Menu Principal", JOptionPane.QUESTION_MESSAGE);
+
+            //tratamentos que vão ser padrões abaixo
+            if (menuResposta == null){
+                return;
+            }
+
+            menuResposta = menuResposta.trim();
+            if (menuResposta.isEmpty()){
+                JOptionPane.showMessageDialog(null,"Você não digitou nada!");
+                continue;
+            }
+
+            int menuRespostaInt = Integer.parseInt(menuResposta);
+
+            switch (menuRespostaInt){
+
+                case 1:
+
+                    break;
+
+                case 2:
+
+                    break;
+
+                case 3:
+
+                    break;
+
+                case 0:
+                    loop = 0;
+                    break;
+
+                default:
+                    JOptionPane.showMessageDialog(null, "Opção inválida! Tente novamente.");
+                    break;
+            }
+        } while (loop != 0);
     }
 
-    public void menuVeiculo (JOptionPane jOptionPane){
-        //aqui vai ter opcao 1 - cadastro, opcao 2 - listar
+    public void menuVeiculo (){
+        int loop = -1;
+
+        do {
+            String menuResposta = JOptionPane.showInputDialog(null, Layouts_JOptionPane.menuVeiculo, "Menu Principal", JOptionPane.QUESTION_MESSAGE);
+
+            //tratamentos que vão ser padrões abaixo
+            if (menuResposta == null){
+                return;
+            }
+
+            menuResposta = menuResposta.trim();
+            if (menuResposta.isEmpty()){
+                JOptionPane.showMessageDialog(null,"Você não digitou nada!");
+                continue;
+            }
+
+            int menuRespostaInt = Integer.parseInt(menuResposta);
+
+            switch (menuRespostaInt){
+
+                case 1:
+
+                    break;
+
+                case 2:
+
+                    break;
+
+                case 3:
+
+                    break;
+
+                case 0:
+                    loop = 0;
+                    break;
+
+                default:
+                    JOptionPane.showMessageDialog(null, "Opção inválida! Tente novamente.");
+                    break;
+            }
+        } while (loop != 0);
     }
 
 
