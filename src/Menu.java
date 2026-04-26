49245cb1e6e0863e278cb1cd7aa22de82b61bb1e
warlley <euwarlley@dominiolechuga.com>
2026-04-26 01:16:40 -0300

    adicionei instancia de metodos e ja comecei a testar os metodos (apenas layout e invocaçao dos mesmos

--- src/Menu.java
+++ src/Menu.java
@@ -2,42 +2,15 @@ import javax.swing.*;
 
 public class Menu {
 
-    //layouts para o joptionpane
-    static final String menuPrincipal = "————— LOCADORA DE VEÍCULOS —————\n" +
-            "Escolha qual painel deseja utilizar:\n" +
-            "[1] - Cliente\n" +
-            "[2] - Funcionário\n" +
-            "[3] - Veículos\n" +
-            "[0] - Sair\n";
-
-    static final String menuCliente = "————— LOCADORA DE VEÍCULOS —————\n"+
-            "— Painel do Cliente —\n" +
-            "[1] - Cadastrar-se\n" +
-            "[2] - Listar clientes\n" +
-            "[3] - Verificar cliente\n" +
-            "[0] - Voltar ao menu\n";
-
-    static final String menuFuncionario = "————— LOCADORA DE VEÍCULOS —————\n"+
-            "— Painel do Funcionário —\n" +
-            "[1] - Admissão de funcionário\n" +
-            "[2] - Listar funcionários\n" +
-            "[3] - Remover funcionário\n" +
-            "[0] - Voltar\n";
-
-    static final String menuVeiculo = "————— LOCADORA DE VEÍCULOS —————\n"+
-            "— Painel de Veículos —\n" +
-            "[1] - Cadastrar veículo\n" +
-            "[2] - Listar frota\n" +
-            "[3] - Pesquisar por Placa\n" +
-            "[0] - Voltar\n";
-
-
     public static void main (String[] args){
         int opcaoLoop = -1;
 
+        Metodos metodos = new Metodos();
+
         do {
             try {
-                String entradaMenu = JOptionPane.showInputDialog(null, menuPrincipal, "Menu Principal", JOptionPane.QUESTION_MESSAGE);
+                String entradaMenu = JOptionPane.showInputDialog(null, Layouts_JOptionPane.menuPrincipal, "Menu Principal", JOptionPane.QUESTION_MESSAGE);
+
                 // ele vem primeiro caso o user cancele a açao ou aperte no x da janela
                 if (entradaMenu == null){
                     return;
@@ -56,15 +29,15 @@ public class Menu {
                 switch (entradaMenuInt) {
 
                     case 1:
-                        System.out.println("Menu cliente");
+                        metodos.menuCliente();
                         break;
 
                     case 2:
-                        System.out.println("Menu funcionario");
+                        metodos.menuFuncionario();
                         break;
 
                     case 3:
-                        System.out.println("teste3");
+                        metodos.menuVeiculo();
                         break;
 
                     case 0:
